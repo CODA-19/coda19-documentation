a465f189833e901cfdee5d1e050502c073e76eac
Kip Brown <kip.brown.chum@ssss.gouv.qc.ca>
2020-08-27 14:51:47 -0400

    Create Observation-PCR.js
    Create JSON Template for PCR data. Almost identical to other observations (vital signs and labs) data

---
+++ data-structure/templates/Observation-PCR.js
@@ -0,0 +1,59 @@
+{
+  
+  // The type of resource
+  "resourceType" : "Observation",
+  
+  // Each resource entry needs a unique id for the ndjson bulk upload. 
+  "id": "f002",
+  
+  // The status of the observation
+  "status": "resulted",
+  
+  // Time of the observation
+  "effectiveDateTime": "YYYY-MM-DDThh:mm:ss+zz:zz",
+    
+  // Time result issued
+  "issued": "YYYY-MM-DDThh:mm:ss.sss+zz:zz",
+  
+  // Patient associated with the observation
+  "subject": {
+    "reference": "Patient/3294843"
+  },
+  
+  // Clinical episode associated with the observation (if possible)
+  "encounter": {
+    "reference": "Encounter/2314234"
+  },
+  
+  // LOINC code for the observation that was made (LOINC "code" field to be omitted until coding/categorization completed)
+  "code": {
+    "coding": [
+      {
+        "system": "http://loinc.org",
+        "code": "94531-1",
+        "display": "RT-PCR tests (respiratory specimen)"
+      }
+    ],
+    "text": "SARS-CoV-2 RT-PCR"
+  },
+  
+  // SNOMED code for the body site used (SNOMED "code" field to be omitted until coding/categorization completed) 
+  "bodySite": {
+    "coding": [
+      {
+        "system": "http://snomed.info/sct",
+        "code": "258500001",
+        "display": "Nasopharyngeal swab (specimen)"
+      }
+    ]
+  },
+  
+  // Value and units of measure
+  "valueCodeableConcept": {
+    "coding": [
+      {
+        "system": "http://snomed.info/sct",
+        "code": "10828004",
+        "display": "Positive (qualifier value)"
+      }
+}
