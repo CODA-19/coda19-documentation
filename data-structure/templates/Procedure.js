b6ae5ffbbee3b1da88d4b5e0428980796378c66d
Kip Brown <kip.brown.chum@ssss.gouv.qc.ca>
2020-08-28 16:27:58 -0400

    Create Procedure.js

---
+++ data-structure/templates/Procedure.js
@@ -0,0 +1,31 @@
+{
+  // The type of resource
+  "resourceType" : "Procedure",
+  
+  // Each resource entry needs a unique id for the ndjson bulk upload. 
+  "id": "f002",
+  
+  "subject": {
+		         "reference": "Patient/pat1",
+             },
+  
+  "code": {
+		"coding": [
+		  {
+			"system": "http://snomed.info/sct",
+			//snomed code to be assigned. Omit if not present
+			"code": "40617009",
+			"display": "Artificial respiration (procedure)"
+		  }
+		],
+		"text": "Mechanical Ventilation"
+	  },
+	  
+  "performedPeriod": {
+    "start": "YYYY-MM-DDThh:mm:ss+zz:zz",
+    "end": "YYYY-MM-DDThh:mm:ss+zz:zz"
+  },
+   
+  "status": "completed"
+
+}  
